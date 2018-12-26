2831a90da9839006a547f60b6b3a26266e6550b3
Sundagy <mblinenkov@yandex.ru>
2018-12-27 02:41:06 +0300

    QILIN-7: add font and styles for auth

--- source/app/login/Login.vue
+++ source/app/login/Login.vue
@@ -1,26 +1,39 @@
 <i18n>
 {"en": {
-    "title": "Login",
-    "login_name": "Email address:",
-    "login_place": "Enter email",
+    "title": "Protocol One Qilin Account",
+    "submit_btn": "Log in",
+    "login_name": "Enter Email",
+    "login_place": "Email address",
     "pass_name": "Password",
     "pass_place": "Password",
-    "not_found": "User not found"
+    "not_found": "User not found",
+    "body": "By having a Qilin account, you can import or add your games to Qilin Data Hub ecosystem and use amazing distribution abilities. Sign up in just seconds.",
+
+
+    "---":""
 },
 "ru": {
-    "not_found": "Пользователь не найден",
-    "title": "Авторизация",
+    "title": "Protocol One Qilin Account",
     "login_name": "Почта:",
     "login_place": "Укажите Email",
     "pass_name": "Пароль",
-    "pass_place": "Пароль"
+    "pass_place": "Пароль",
+    "not_found": "Пользователь не найден",
+    "body": "By having a Qilin account, you can import or add your games to Qilin Data Hub ecosystem and use amazing distribution abilities. Sign up in just seconds.",
+
+    "---":""
 }}
 </i18n>
 
 <template>
     <b-modal :id="id"
              :title="$t('title')"
+             class="qilin-modal"
+             centered
+             hide-header-close
              @ok.prevent="clickOk">
+
+        <p style="padding-top: 0;">{{ $t('body') }}</p>
 
         <b-form @submit.prevent="submit" method="post">
             <b-form-group :label="$t('login_name')" label-for="login">
@@ -31,17 +44,30 @@
                               :placeholder="$t('login_place')">
                 </b-form-input>
             </b-form-group>
-            <b-form-group :label="$t('pass_name')" label-for="password">
+            <b-form-group label-for="password">
                 <b-form-input id="password"
                               type="password"
                               v-model="form.password"
                               required
                               :placeholder="$t('pass_place')">
                 </b-form-input>
+
+                <b-tooltip target="password" placement="right">
+                    Hello <strong>World!</strong>
+                </b-tooltip>
             </b-form-group>
+
+            <small class="form-text">
+                Don`t have an account? <a href="/">Sign Up</a> | <a href="/">Reset password</a>
+            </small>
+
+
             <b-button type="submit" variant="primary" ref="submit" v-show="false">Submit</b-button>
         </b-form>
 
+        <div slot="modal-footer" class="w-100">
+            <b-btn size="sm" class="float-right" variant="primary" @click="show=false">{{ $t('submit_btn') }}</b-btn>
+        </div>
     </b-modal>
 </template>
 
@@ -55,8 +81,8 @@
         props: ['id'],
         data: () => ({
             form: {
-                login: 'admin',
-                password: 'password',
+                login: '',
+                password: '',
             },
         }),
         methods: {
--- source/layouts/main/bootstrap.ts
+++ source/layouts/main/bootstrap.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue'
 import './bootstrap.scss'
-//import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import 'bootstrap/dist/js/bootstrap.js'
 
 Vue.use(BootstrapVue);
 
