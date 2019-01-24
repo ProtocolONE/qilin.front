9c307b962da1b591270b273efcce7fabc82b728f
Pavel Kokovin <p.kokovin@office.ngs.ru>
2019-01-24 18:10:50 +0700

    QILIN-43: Declare types for vue-clickaway

--- source/types.d.ts
+++ source/types.d.ts
@@ -1,13 +1,28 @@
+interface Directive {
+  bind(): void;
+  update(): void;
+  unbind(): void;
+}
+interface onClickaway {
+  onClickaway: Directive;
+}
+interface Mixin {
+  directives: onClickaway;
+}
+
 /**
  * @TODO For main.ts we need type of vue instances, but
  * vue declaring generate lint error.
  */
 declare module '*.vue' {
-    import Vue from 'vue';
-    export default Vue;
+  import Vue from 'vue';
+  export default Vue;
 }
 
-declare module '*.json' {
-    const value: any;
-    export default value;
+declare module 'vue-clickaway' {
+  const directive: Directive;
+  const mixin: Mixin;
+  const version: string;
+
+  export { directive, mixin, version };
 }
