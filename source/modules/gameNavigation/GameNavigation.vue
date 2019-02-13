f570e0c44af00b392ef59f914e7e9ab936f18abf
Pavel Kokovin <p.kokovin@office.ngs.ru>
2019-02-13 19:04:10 +0700

    Add dummy for gameNavigation module

---
+++ source/modules/gameNavigation/GameNavigation.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="game-nav">
+  
+</div>
+</template>
+
+<script type="ts">
+import Vue from 'vue'
+import { PageHeader } from '@protocol-one/ui-kit';
+
+/**
+ * @TODO - Create navigation game page
+ * @see https://www.figma.com/file/JaTkm9DB00Wne8u9ZzqjIVXS/Qilin-Ui%5CUX?node-id=249%3A101
+ */
+export default Vue.extend({
+  components: { PageHeader },
+})
+</script>
+
+<style scoped lang="scss">
+.game-nav {
+
+}
+</style>
