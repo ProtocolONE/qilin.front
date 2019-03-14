96937ac7b0c5d69fe95132830acc684b673010bb
Pavel Kokovin <p.kokovin@office.ngs.ru>
2019-03-14 15:17:12 +0700

    Remove extra components, fix Navbar

---
+++ source/components/Auth.vue
@@ -0,0 +1,69 @@
+<template>
+<UiModal @close="$emit('close')">
+  <iframe
+    slot="main"
+    class="auth1"
+    height="100%"
+    width="100%"
+    :src="frameSrc"
+  ></iframe>
+</UiModal>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { includes } from 'lodash-es';
+import { UiModal } from '@protocol-one/ui-kit';
+
+interface Auth1Message {
+  access_token: string;
+  success: boolean;
+  error: string;
+  expires_in: number;
+}
+
+export default Vue.extend({
+  components: { UiModal },
+  props: {
+    type: {
+      default: 'login',
+      type: String,
+      validator(value: string) {
+        return includes(['login', 'register'], value);
+      },
+    },
+  },
+  computed: {
+    frameSrc() {
+      // @TODO - change 'login' to this.type, when we will have frame for reg
+      return '/auth1/login';
+    }
+  },
+  created () {
+    this.listenFrame();
+  },
+  methods: {
+    listenFrame () {
+      window.addEventListener('message', (e) => {
+        try {
+          const { access_token, success }: Auth1Message = JSON.parse(e.data);
+
+          if (access_token && success) {
+            localStorage.setItem('accessToken', access_token);
+            location.reload();
+          }
+        } catch (e) {}
+      });
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.auth1 {
+  display: block;
+  width: 400px;
+  height: 500px;
+  border: none;
+}
+</style>
