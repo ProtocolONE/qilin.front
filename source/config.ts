4858802b9ce1d375e3a8c062af830abd50ba0c47
Pavel Kokovin <pavel.kokovin@protocol.one>
2019-03-25 15:03:22 +0700

    Fix env for admin api value

--- source/config.ts
+++ source/config.ts
@@ -1,5 +1,5 @@
 export default {
-  adminApi: process.env.QILIN_API || 'http://127.0.0.1:3001/admin/api/v1',
+  adminApi: process.env.QILIN_ADMIN_API || 'http://127.0.0.1:3001/admin/api/v1',
   api: process.env.QILIN_API || 'http://127.0.0.1:3001/api/v1',
   imaginary_api: process.env.IMAGINARY_API || 'http://127.0.0.1:9000',
   static: process.env.QILIN_STATIC || 'https://static.protocol.one/qilin',
